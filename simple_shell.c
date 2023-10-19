ab53e43feeaac41fc21dbbb5c5694180c08e2d42
math198512 <57964579+math198512@users.noreply.github.com>
2023-10-19 21:31:51 +0100

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -1,75 +1,33 @@
 #include "main.h"
 
 /**
-* execute - Execute a command.
-* @command: The command to execute.
-* @status: Pointer to the status variable to be updated.
-*/
-void execute(char **command, int *status)
-{
-
-	pid_t pid = fork();
-
-	if (pid == 0)
-	{
-		if (execve(command[0], command, environ) == -1)
-		{
-			perror("execve error");
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		wait(status);
-	}
-}
-
-
-/**
-* main - simple_shell.
-* @ac: arguments count.
-* @argv: arguments vector.
-*
-* Return: Nothing.
-*/
+ * main - simple_shell.
+ * @ac: arguments count.
+ * @argv: arguments vector.
+ *
+ * Return: Nothing.
+ */
 
 int main(int ac, char **argv)
 {
-	int status, i;
+        int status = 0, i;
+        char **cmd = NULL, *line = NULL;
+        (void) ac;
 
-	char **array, *token, *buffer;
-
-	(void) ac;
-	(void) argv;
-
-	status = 0;
-	i = 0;
-	array = malloc(sizeof(char *) * 1024);
-	while (1)
-	{
-		buffer = my_getline();
-		if (buffer == NULL)
-		{
-			if (isatty(STDIN_FILENO))
-				write(1, "\n", 1);
-			return (status);
-		}
-		token = strtok(buffer, " \t\n");
-		while (token)
-		{
-			array[i] = token;
-			token = strtok(NULL, " \t\n");
-			i++;
-		}
-		array[i] = NULL;
-		if (_strcmp(array[0], "exit") == 0)
-		{
-			free2d(array);
-			exit(EXIT_SUCCESS);
-		}
-		execute(array, &status);
-		i = 0;
-		free(buffer);
-		free(array);
-	}
+        for (;;)
+        {
+                line = my_getline();
+                if (line == NULL)
+                {
+                        if (isatty(STDIN_FILENO))
+                                write(1, "\n", 1);
+                        return (status);
+                }
+                cmd = my_strtok(line);
+                if (cmd == NULL)
+                        continue;
+                free2d(cmd);
+                status = my_exec;
+                i++;
+        }
 }
