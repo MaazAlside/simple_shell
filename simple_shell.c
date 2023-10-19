d30934882eb768303c6a626376967ffb164115ed
math198512 <thoraichi@gmail.com>
2023-10-19 13:17:55 -0700

    test

--- simple_shell.c
+++ simple_shell.c
@@ -10,50 +10,24 @@
 
 int main(int ac, char **argv)
 {
-	pid_t pid;
-	int status, i;
-	char **array, *token, *buffer;
+	int status = 0, i;
+	char **cmd = NULL, *line = NULL;
 	(void) ac;
-	(void) argv;
 
-	status = 0;
-	i = 0;
-	array = malloc(sizeof(char *) * 1024);
-	while (1)
+	for (;;)
 	{
-		buffer = my_getline();
-		if (buffer == NULL)
+		line = my_getline();
+		if (line == NULL)
 		{
 			if (isatty(STDIN_FILENO))
 				write(1, "\n", 1);
 			return (status);
 		}
-		token = strtok(buffer, " \t\n");
-		while (token)
-		{
-			array[i] = token;
-			token = strtok(NULL, " \t\n");
-			i++;
-		}
-		array[i] = NULL;
-		if (_strcmp(array[0], "exit") == 0)
-		{
-			free2d(array);
-			exit(EXIT_SUCCESS);
-		}
-		pid = fork();
-		if (pid == 0)
-		{
-			if (execve(array[0], array, environ) == -1)
-				perror("execve error");
-
-		}
-		else
-		{
-			wait(&status);
-		}
-		i = 0;
-		free(buffer);
-		free(array);
+		cmd = my_strtok(line);
+		if (cmd == NULL)
+			continue;
+		free2d(cmd);
+		status = my_exec;
+		i++;
 	}
 }
