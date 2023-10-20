1fe34d04f2cbc5beab6a80a8cde7a71b3a3a865a
math198512 <thoraichi@gmail.com>
2023-10-20 14:35:09 -0700

    env

---
+++ _strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the length of a string
+ * @str: string
+ *
+ *Return: returns lenght;
+ */
+
+int _strlen(char *str)
+{
+	int count, inc;
+
+	inc = 0;
+	for (count = 0; str[count] != '\0'; count++)
+		inc++;
+	return (inc);
+}
