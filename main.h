ea7b7a1fc979e0f6c912b326782859ab162efcd8
math198512 <thoraichi@gmail.com>
2023-10-20 04:45:19 -0700

    tasks 0 1 2

--- main.h
+++ main.h
@@ -2,9 +2,12 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <stdlib.h>
 #include <string.h>
 
